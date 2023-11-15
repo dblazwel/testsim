ab8c57b8a8048389d9161d14060ea2c27094e599
David Blázquez <dblazquezg99@gmail.com>
2023-11-15 20:32:04 +0100

    Add controler to the script

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,7 +1,19 @@
 
 # DBG - Script para ejecutar la pipeline
+# Comandos que solo deben ejecutarse una vez:
 
-mkdir -p res/genome
-wget -O res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
-gunzip -k ecoli.fasta.gz 
+# Obtener el genoma
+ mkdir -p res/genome
+ wget -O res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
+ gunzip -k ecoli.fasta.gz 
 
+# Obtener el índice
+ echo "Running STAR index..."
+ mkdir -p res/genome/star_index
+ STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/genome/star_index/ --genomeFastaFiles res/genome/ecoli.fasta --genomeSAindex
+
+# Controlador: Analizar cada una de las muestras
+for sampleid in $(ls data/*fastq.gz | cut -d'_' -f1 |cut -d'/' -f2 | sort | uniq);
+do
+	echo bash scripts/analyse_sample.sh $sampleid;
+done
